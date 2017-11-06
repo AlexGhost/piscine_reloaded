a97284f4abab231d4af9e8f2648a222cc7831e04
acourtin <acourtin@student.42.fr>
2017-11-06 18:43:35 +0100

    modif de ex20

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -5,36 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/06 17:29:27 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/06 17:36:46 by acourtin         ###   ########.fr       */
+/*   Created: 2017/11/06 18:43:23 by acourtin          #+#    #+#             */
+/*   Updated: 2017/11/06 18:43:25 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int		ft_strcount(char *src)
-{
-	int i;
-
-	i = 0;
-	while (src[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
 char	*ft_strdup(char *src)
 {
-	char	*res;
+	char	*newptr;
 	int		i;
+	int		taille;
 
-	res = malloc(sizeof(char) * (ft_strcount(src) + 1));
 	i = 0;
-	while (src[i])
+	taille = 0;
+	while (src[taille] != '\0')
 	{
-		res[i] = src[i];
-		i++;
+		taille += 1;
 	}
-	return (res);
+	newptr = (char*)malloc(sizeof(*newptr) * taille + 1);
+	while (src[i] != '\0')
+	{
+		newptr[i] = src[i];
+		i += 1;
+	}
+	newptr[i] = '\0';
+	return (newptr);
 }
