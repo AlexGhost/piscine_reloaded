de3cd80de2a3f31e85accac8b857cabc1522080a
acourtin <acourtin@student.42.fr>
2017-11-06 19:12:37 +0100

    modif de 13

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 15:43:14 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/06 15:57:13 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/11/06 19:12:24 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int		ft_recursive_factorial(int nb)
 {
 	if (nb == 0)
 		return (1);
-	else if (nb < 0)
+	else if (nb < 0 || nb > 12)
 		return (0);
 	return (nb * ft_recursive_factorial(nb - 1));
 }
